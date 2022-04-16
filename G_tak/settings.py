6564d5be2b72b98c53924b8b7f24c00cbfc1068d
IpoLa <akramchat@hotmail.com>
2022-04-16 02:54:36 +0100

    heroku

--- G_tak/settings.py
+++ G_tak/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_heroku
 import datetime
 from pathlib import Path
 
@@ -156,3 +157,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
