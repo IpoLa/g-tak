3df3f4096373c6c251a029d8e2630be40ccef7d7
IpoLa <akramchat@hotmail.com>
2022-04-17 19:03:07 +0100

    category

--- gtakapp/admin.py
+++ gtakapp/admin.py
@@ -2,12 +2,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from gtakapp.models import Restaurant, Customer, Driver, Meal, Order, OrderDetails, phoneModel
+from gtakapp.models import Restaurant, Customer, Driver, Meal, Category, Order, OrderDetails, phoneModel
 
 admin.site.register(Restaurant)
 admin.site.register(Customer)
 admin.site.register(phoneModel)
 admin.site.register(Driver)
 admin.site.register(Meal)
+admin.site.register(Category)
 admin.site.register(Order) 
 admin.site.register(OrderDetails)
