a3b727c152163784f383905de02c17fdf40706ee
IpoLa <akramchat@hotmail.com>
2022-04-19 17:47:01 +0100

    static url

--- G_tak/urls.py
+++ G_tak/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from rest_framework_jwt.views import obtain_jwt_token
 from gtakapp.apis import APIHomeView, \
         MealListAPIView, \
